eefa12b0d1b37d82ced601bee644b14e0cb19de3
PajarKharisma <pajarkharisma@hotmail.com>
2021-10-22 14:52:04 +0700

    update app.py

--- app.py
+++ app.py
@@ -5,5 +5,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World'
 
+@app.route('/biodata')
+def biodata():
+    bio = 'nama : Pajar <br> alamat : Bandarlampung'
+    return bio
+
 if __name__ == '__main__':
     app.run()
